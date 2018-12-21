908676d0cb208fad0c279425753f129cb85fbe85
Nadeem Yousuf <Nadeem.Yussuf@gmail.com>
2018-12-21 10:53:21 +0530

    Added Calculator Class with Add method

---
+++ Calculator/ScCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class ScCalculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
